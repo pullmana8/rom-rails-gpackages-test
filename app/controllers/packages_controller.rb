3ce38f6f1f4839eda87ff53d6bc7fa82ef8eb3a9
Antonette Caldwell <antonettecaldwell@tutanota.com>
2019-05-07 17:01:32 -0400

    Added Categoy and Package controllers

---
+++ app/controllers/packages_controller.rb
@@ -0,0 +1,10 @@
+class PackagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'packages/index'
+  get 'packages/show'
+  get 'packages/search'
+  get 'pckages/index'
+  get 'pckages/show'
+  get 'pckages/search'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
